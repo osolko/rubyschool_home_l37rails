fafe00272516b4ec5a227bb01b50a2fd9e2e2b24
osolko <o.savka@gmail.com>
2019-06-03 09:40:56 +0300

    save contact on submit l38_1:09:05

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -5,7 +5,17 @@ class ContactsController < ApplicationController
 	end
 		
 	def create
-	 
+#		@contact = Contact.new(params[:contact])
+		@contact = Contact.new(contact_params)
+		@contact.save
+
 	end	
 	
+
+	private
+
+	def contact_params
+		params.require(:contact).permit(:email, :message)
+	end
+
 end
