bf313efa706638a723a3020b2661d0d0256237f2
osolko <o.savka@gmail.com>
2019-06-03 08:26:06 +0300

    add contacts controller + view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+#  get 'home/contacts'
   get 'home/index'
 
   resources :articles
+  
+  resource :contacts, only: [:new, :create]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
