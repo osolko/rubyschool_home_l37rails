9652205b9b15688c2cb6ec5f242dfd6fdb47686e
osolko <o.savka@gmail.com>
2019-06-04 09:02:49 +0300

    add terms

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,8 @@ Rails.application.routes.draw do
   resource :contacts, only: [:new, :create], path_names: { :new => ''}
   #get 'contacts' => 'contacts#new'
 
+  resource :terms
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
