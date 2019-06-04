2d8c9628595889b26c07286568d566795db42ef3
osolko <o.savka@gmail.com>
2019-06-04 09:18:46 +0300

    add about (controller abouts)

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
 
   resource :terms
 
+  resource :about
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
