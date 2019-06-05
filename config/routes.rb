8ec4542e389caef078c20a30395810ccb8198235
osolko <o.savka@gmail.com>
2019-06-05 08:11:29 +0300

    test controler check NOT checks

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,9 @@ Rails.application.routes.draw do
 
   resource :about
 
+  resource :check
+
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
