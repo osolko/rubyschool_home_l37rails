6863e2b4d0c03b060c67159ad99bda5f97752f15
osolko <o.savka@gmail.com>
2019-06-05 08:34:30 +0300

    implement article#show used id

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,6 @@
 class ArticlesController < ApplicationController
 
 	def new
-	
 	end
 
 
@@ -9,10 +8,16 @@ class ArticlesController < ApplicationController
 		@article = Article.new(article_params)
 		if @article.valid?
 			 @article.save
+			 redirect_to @article
 		else
 			render action: 'new'
 		end		
 	end	
+
+
+	def show
+		@article = Article.find(params[:id])
+	end
 	
 
 	private
