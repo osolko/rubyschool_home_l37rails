656720da314e4fe2086a6cce5bc41e317f2f4381
osolko <o.savka@gmail.com>
2019-06-06 07:59:15 +0300

    def index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,16 @@
 class ArticlesController < ApplicationController
 
+	
+	def index
+		@articles = Article.all
+	end
+
+
+	def show
+		@article = Article.find(params[:id])
+	end
+
+
 	def new
 	end
 
@@ -15,10 +26,6 @@ class ArticlesController < ApplicationController
 	end	
 
 
-	def show
-		@article = Article.find(params[:id])
-	end
-	
 
 	private
 
