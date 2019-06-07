69a38c92c40d39b52088427d97d0848a2062a21b
osolko <o.savka@gmail.com>
2019-06-07 07:39:06 +0300

    rm controller check UNUSeD only as test

--- app/controllers/check_controller.rb
+++
@@ -1,2 +0,0 @@
-class CheckController < ApplicationController
-end
--- config/routes.rb
+++ config/routes.rb
@@ -16,9 +16,6 @@ Rails.application.routes.draw do
 
   #resource :about
 
-  resource :check
-
-
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
