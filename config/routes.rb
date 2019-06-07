03e8b8d7ea6ce81f7ed12a48a1caa520dfdb0bfa
osolko <o.savka@gmail.com>
2019-06-07 09:40:19 +0300

    remove not needed resources for #comments

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'home/index'
 
   resources :articles do
-  	resources :comments
+  	resources :comments,  only: [:new, :create]
   end
   
   resource :contacts, only: [:new, :create], path_names: { :new => ''}
