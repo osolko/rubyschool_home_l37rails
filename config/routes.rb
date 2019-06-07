a6fdc8396a208213d92bef7f70bede89e7b99ac8
osolko <o.savka@gmail.com>
2019-06-07 08:59:09 +0300

    add routes for comments

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,18 @@ Rails.application.routes.draw do
 #  get 'home/contacts'
   get 'home/index'
 
-  resources :articles
+  resources :articles do
+  	resources :comments
+  end
   
   resource :contacts, only: [:new, :create], path_names: { :new => ''}
   #get 'contacts' => 'contacts#new'
 
   get 'terms' => 'pages#terms'
-
   get 'about' => 'pages#about'
+
+
+
 
 
   #resource :terms  #we use another one controller pages and stati page see upper get 'terms' ... etc
