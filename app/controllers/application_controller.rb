a3c2629a4e9043b8120682a4eba730a48bfd1e8b
osolko <o.savka@gmail.com>
2019-06-14 07:48:10 +0300

    fix configure_permitted_parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
 	protected
 	
 	def configure_permitted_parameters
-	   devise_parameter_sanitizer.for(:sign_up) << :username #add symbol :username to Array
+#	  devise_parameter_sanitizer.for(:sign_up) << :username #add symbol :username to Array
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
 	end 
 end
