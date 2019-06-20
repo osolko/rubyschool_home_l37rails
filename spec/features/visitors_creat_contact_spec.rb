10bae15d345f11e14aea3601b98e43bf56b8a2df
osolko <o.savka@gmail.com>
2019-06-20 09:11:19 +0300

    upd tests guest create contacts

--- spec/features/visitors_creat_contact_spec.rb
+++ spec/features/visitors_creat_contact_spec.rb
@@ -16,4 +16,23 @@ feature "Contact creattion" do
 		
 		expect(page).to have_content I18n.t('contacts.contact_us')
 	end
+
+
+	scenario "allow a guest to create contact" do
+    visit '/contacts'
+
+    fill_in :contact_email, with: 'user@example.com'
+    fill_in :contact_message, with: 'something'
+		
+		click_button 'Send message'
+
+
+		expect(page).to have_content 'Thank you page'
+	end
+
+
 end
+
+
+
+
