0705bf51bbf999605c737d2348857badafaa47f1
osolko <o.savka@gmail.com>
2019-06-20 08:01:30 +0300

    check title lenght

--- spec/model/article_spec.rb
+++ spec/model/article_spec.rb
@@ -7,6 +7,8 @@ describe Article, type: :model do
   	it { should validate_presence_of :text }
 	end
 
+
+
 # could use describe-end for example "association" to make readeble
   it { should have_many :comments }
 
@@ -19,6 +21,17 @@ describe Article, type: :model do
   	end
 	end
 
+  describe "#subject" do
+  	it "returns the article title" do
+  		article = create(:article, title: 'Lorem Ipsum')
+
+  		expect(article.subject.length).to be(11)
+
+  	end
+	end
+
+
+	
 	describe "#last_comment" do
 		it "returns the last comment" do
 			#create article with comments
@@ -28,5 +41,6 @@ describe Article, type: :model do
 			expect(article.last_comment.body).to eq "comment body 3"
 		end
 	end
+
 
 end
