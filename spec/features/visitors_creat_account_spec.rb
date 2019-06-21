cbbb422bdc72fbe893bd93fc5b70482b50f6ec18
osolko <o.savka@gmail.com>
2019-06-21 08:23:28 +0300

    sign_up as method

--- spec/features/visitors_creat_account_spec.rb
+++ spec/features/visitors_creat_account_spec.rb
@@ -1,17 +1,15 @@
 require 'rails_helper'
+require 'support/session_helper'
 
 feature "Account creation" do
 	scenario "allow guest to create contact" do
-		visit new_user_registration_path
-
-		fill_in :user_email, with: 'user1@example.com'
-		fill_in :user_username, with: 'test_Mike'
-		fill_in :user_password, with: '123456'
-		fill_in :user_password_confirmation, with: '123456'
-
-		click_button 'Sign up'
+		
+		# I call the method
+		sign_up
 
 		expect(page).to have_content I18n.t('devise.registrations.signed_up')
 	end
 
 end
+
+
